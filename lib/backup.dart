49c3a3df802bc5d8e16561b32214bd8e719e64af
Sigit Kurniawan <sigitkurniawanmtsn@gmail.com>
2025-07-22 09:06:27 +0700

    feat: penambahan kode program

---
+++ lib/backup.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter ',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('TI-RPL'),
+        ),
+        body: Center(
+          child: Text(
+            'Competence for competition!',
+            textDirection: TextDirection.ltr,
+          ),
+        ),
+      ),
+    );
+  }
+}
